d3e3d768c1b199240065e9ec22eddb5eb5b23520
Hayley Painter <hhawkins2012@gmail.com>
2014-08-02 20:26:40 -0500

    Make upload page display text

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,7 @@
- function start(response){
+function start(response, postData) {
     console.log("Request handler 'start' was called.");
 
-     var body = '<html>'+
+    var body = '<html>'+
      '<head>'+
      '<meta http-equiv="Content-Type" content="text/html; '+
      'charset=UTF-8" />'+
@@ -14,17 +14,17 @@
      '</body>'+
      '</html>';
 
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(body);
-    response.end();
-}
+     response.writeHead(200, {"Content-Type": "text/html"});
+     response.write(body);
+     response.end();
+ }
 
-function upload(response){
-    console.log("Request handler 'upload' was called."); 
-    response.writeHead(200, {"Content-Type": "text/plain"}); 
-    response.write("Hello Upload");
-    response.end();
-} 
+ function upload(response, postData) {
+     console.log("Request handler 'upload' was called.");
+     response.writeHead(200, {"Content-Type": "text/plain"});
+     response.write("You've sent: " + postData);
+     response.end();
+ }
 
-exports.start=start;
-exports.upload=upload;
+ exports.start=start;
+ exports.upload=upload;
