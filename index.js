2ed5b8ff5390b195ab7b871ce3f1178bfbfa2525
Hayley Painter <hhawkins2012@gmail.com>
2014-08-02 20:53:57 -0500

    with mongoDB

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+'usestrict';
+var MongoClient=require('mongodb').MongoClient;
+MongoClient.connect('mongodb://127.0.0.1:27017/accounting', function(err, connection) {
+    var collection = connection.collection('customers'); 
+    collection.insert({'name': 'Jane Doe'}, function(err, count) {
+        collection.find().toArray(function(err, documents) { 
+            console.dir(documents);
+            connection.close();
+        }); 
+    });
+});
+
+
 var server=require("./server");
 var router=require("./router");
 var requestHandlers=require("./requestHandlers");
