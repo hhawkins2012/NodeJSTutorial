62fdac5792e8db6fb71ccde2f374271ab35d7bf0
Hayley Painter <hhawkins2012@gmail.com>
2014-08-02 21:14:07 -0500

    Connected to STIX db in mongodb

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 'usestrict';
 var MongoClient=require('mongodb').MongoClient;
-MongoClient.connect('mongodb://127.0.0.1:27017/accounting', function(err, connection) {
-    var collection = connection.collection('customers'); 
-    collection.insert({'name': 'Jane Doe'}, function(err, count) {
-        collection.find().toArray(function(err, documents) { 
+MongoClient.connect('mongodb://127.0.0.1:27017/stixdb', function(err, connection) {
+    var stixMsgs = connection.collection('messages'); 
+        stixMsgs.find().toArray(function(err, documents) { 
             console.dir(documents);
             connection.close();
-        }); 
-    });
+        });  
 });
 
 
